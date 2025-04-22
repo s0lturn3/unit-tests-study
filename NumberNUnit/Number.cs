ec575103013df12818729503e6482094b4ecabd5
Erick Carvalho <erickzcarvalho25@outlook.com>
2025-04-21 22:12:22 -0300

    projeto nunit adicionado direito dessa vez

--- NumberNUnit/Number.cs
+++ NumberNUnit/Number.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using NumberSystem;
 
-namespace NumberTest
+namespace NumberNUnit
 {
     [TestFixture]
     public class Number
--- NumberNUnit/Properties/AssemblyInfo.cs
+++ NumberNUnit/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associada a um assembly.
-[assembly: AssemblyTitle("NumberTest")]
+[assembly: AssemblyTitle("NumberNUnit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NumberTest")]
+[assembly: AssemblyProduct("NumberNUnit")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
